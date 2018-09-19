d080d7aad35aeeed40cf970314adaea3abeccaa2
Linus Du <linus.du@stoamigo.com>
2018-09-19 19:00:15 +0800

    add html file

--- db/schemas/video.js
+++ db/schemas/video.js
@@ -5,8 +5,8 @@ const videoSchema = new mongoose.Schema({
     category: {type: Number, enum: [1, 2, 3, 4, 5], required: true}, // 1 = 男双， 2 = 混双， 3 = 女双， 4 = 男单， 5 = 女单
     title: {type: String, required: true},
     label: {type: Array, required: true, default: []},
-    gameLocation: {type: String, required: true},
-    gameYear: {type: Number, required: true},
+    location: {type: String, required: true},
+    year: {type: Number, required: true},
     isLatest: {type: Number, enum: [0, 1], default: 0}
 });
 
