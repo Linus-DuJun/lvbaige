b9d90717b4116e883b4b086cdc6276c44cdd1722
Linus-DuJun <dj13808079752@gmail.com>
2018-09-25 22:48:09 +0800

    add html page for reset hot video.

--- routes/videos.js
+++ routes/videos.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 
 router.post("/", function (request, response, next) {
-    console.log(request.body);
+    let action = request.body._a;
+    console.log(action);
+    if ("reset" === action) {
+        //TODO   add logic for update latest videos.
+        response.redirect("./addvideo.html");
+    } else {
+        response.redirect("./addvideo.html");
+    }
 })
 
 
